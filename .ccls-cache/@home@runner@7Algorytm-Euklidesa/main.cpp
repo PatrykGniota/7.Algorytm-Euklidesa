06af4c4030e7a20238a062231abd4bd6db5a96b5
PatrykGniota <120011921+PatrykGniota@users.noreply.github.com>
2023-02-01 13:51:24 +0000

    Zooptymalizowany Algorytm Euklidesa(podfunkcja)

--- .ccls-cache/@home@runner@7Algorytm-Euklidesa/main.cpp
+++ .ccls-cache/@home@runner@7Algorytm-Euklidesa/main.cpp
@@ -1,19 +1,29 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
 int NWD(int x,int y){
-  while(x != y){
+  while(x != 0 && y != 0){
     if(x > y){
-      x = x - y;
+      x = x % y;
     }
     else{
-      y = y - x;
+      y = y % x;
     }
   }
-  return x;
+  while(x * y != 0){
+    if(x > y){
+      x = x % y;
+    }
+    else{
+      y = y % x;
+    }
+  }
+  return x + y;
 }
 
 int main() {
   int a = 12;
   int b = 20;
+  
   cout << "NWD = " << NWD(a,b);
 }
