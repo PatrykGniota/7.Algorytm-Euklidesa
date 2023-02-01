03245838387a766fcceb0ac5c51271f1f93f08c0
PatrykGniota <120011921+PatrykGniota@users.noreply.github.com>
2023-02-01 13:26:09 +0000

    Algorytm Euklidesa

--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@ using namespace std;
 int main() {
   int a = 12;
   int b = 20;
-  for(int NWD = min(a,b); NWD >= 1; NWD--){
-    if(a % NWD == 0 && b % NWD == 0){
-      cout << "NWD = " << NWD << endl;
-      break;
+  while(a != b){
+    if(a > b){
+      a = a - b;
+    }
+    else{
+      b = b - a;
     }
   }
+  cout << "NWD = " << a;
 }
