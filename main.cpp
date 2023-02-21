63b08e1d83908c27a9cc4b656b97046d45655124
PatrykGniota <120011921+PatrykGniota@users.noreply.github.com>
2023-02-21 08:50:22 +0000

    kalkulator

--- main.cpp
+++ main.cpp
@@ -1,40 +1,18 @@
+#include "ulamki.hpp"
 #include <bits/stdc++.h>
 using namespace std;
 
-struct fraction{
-  int numerator;
-  int nominative;
-};
+int main() {
+  kalkulator a;
+  a.u1.numerator = 2;
+  a.u1.nominative = 4;
+  a.u2.numerator = 1;
+  a.u2.nominative = 2;
 
-int nwd(int x,int y){
-  while(x){
-    x = x % y;
-    y = y - x;
-  }
-  return y;
-}
-int nww(int x,int y){
-  return x * y / nwd(x,y);
-}
+  a.add().print();
+  a.substr().print();
+  a.multi().print();
+  a.div().print();
 
-int main(){
-  fraction ulamek1;
-  ulamek1.numerator = 2;
-  ulamek1.nominative = 4;
-
-  fraction ulamek2;
-  ulamek2.numerator = 5;
-  ulamek2.nominative = 7;
-
-  int licznik;
-  int mianownik;
-  
-  licznik = ulamek1.numerator * ulamek2.numerator;
-  mianownik = ulamek1.nominative * ulamek2.nominative;
-  int c = nwd(licznik,mianownik);
-  while(nwd(licznik,mianownik) != 1){
-    licznik = licznik / c;
-    mianownik = mianownik / c;    
-  }
-  cout << licznik << "/" << mianownik;
+  return 0;
 }
